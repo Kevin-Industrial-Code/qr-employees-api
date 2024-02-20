c822ceeefc22167d103d1551707a8067df04a753
kevin <k.basto@industrial-code.com>
2024-02-20 07:12:39 -0600

    feat: add plan to the entities

---
+++ src/core/entities/plan.ts
@@ -0,0 +1,13 @@
+export class Plan {
+    planName : string;
+    clubsLogo: boolean;
+    entryFunction: boolean;
+    multiLocationEntry: boolean;
+    coatFunction: boolean;
+    multiLocationCoatChecker: boolean;
+    bagOrItemChecker: boolean;
+    brakeTime : boolean;
+    allowedBrakes: boolean;
+    dashboard: boolean;
+    guestInfo: boolean;
+}
