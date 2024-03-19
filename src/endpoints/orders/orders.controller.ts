42a9be5024de15541211681d73b54359f354753e
kevin <k.basto@industrial-code.com>
2024-03-19 16:33:25 -0600

    feat: add orders controller

--- src/endpoints/orders/orders.controller.ts
+++ src/endpoints/orders/orders.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { Message } from 'src/core/shared/message';
 import { Exception } from 'src/core/shared/exception';
--- src/endpoints/orders/orders.service.ts
+++ src/endpoints/orders/orders.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrdersRepoService } from 'src/repos/orders-repo/orders-repo.service';
 import { QrRepoService } from 'src/repos/qr-repo/qr-repo.service';
 
